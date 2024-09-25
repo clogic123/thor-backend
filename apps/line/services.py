9bf3250d11650a47e0a5915e956fe1b5a3c2e599
clogic <choiseungil29@gmail.com>
2024-09-25 16:35:56 +0900

    create line 수정

--- apps/line/services.py
+++ apps/line/services.py
@@ -9,16 +9,6 @@ class LineService:
     @async_atomic
     async def create(self, data: CreateLineRequestSchema):
         line = await FoodProcessLine.objects.acreate(**data.dict())
-        next_step = None
-        for index, name in reversed(Constants.STEP_LIST):
-            step = await FoodProcessLineStep.objects.acreate(
-                name=name,
-                order=index,
-                line_id=line.id,
-                enabled=True,
-                next_step=next_step,
-            )
-            next_step = step
         return line
 
 
